f9da483b971cee794b4d45ee873d050245c08857
Tiago Araujo <a.tiago@ipvc.pt>
2025-06-18 19:22:31 +0100

    parte 4

---
+++ components/input/keyboard-input-component.js
@@ -0,0 +1,25 @@
+import { InputComponent } from "./input-component.js";
+
+export class KeyboardInputComponent extends InputComponent {
+    #cursorKeys;
+    #inputLocked;
+
+    constructor(scene){
+        super();
+        this.#cursorKeys = scene.input.keyboard.createCursorKeys();
+        this.#inputLocked = false;
+    }
+
+    update() {
+        if (this.#inputLocked) {
+            this.reset();
+            return;
+        }
+
+        this._up = this.#cursorKeys.up.isDown;
+        this._down = this.#cursorKeys.down.isDown;
+        this._left = this.#cursorKeys.left.isDown;
+        this._right = this.#cursorKeys.right.isDown;
+        this._shoot = this.#cursorKeys.space.isDown;
+    }
+}
--- src/objects/player.js
+++ src/objects/player.js
@@ -1,17 +1,41 @@
+import { KeyboardInputComponent } from '../../components/input/keyboard-input-component.js';
+import { HorizontalMovementComponent } from '../../components/movement/horizontal-movement-component.js';
+import * as CONFIG from '../../src/config.js';
+
 export class Player extends Phaser.GameObjects.Container {
+    #keyboardInputComponent;
+    #horizontalMovementComponent;
     #shipSprite;
     #shipEngineSprite;
     #shipEngineThrustSprite;
 
     constructor(scene) {
-        super(scene, scene.scale.width/2 , scene.scale.height - 32, []);
-            
+        super(scene, scene.scale.width / 2, scene.scale.height - 32, []);
+
         this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+        this.body.setSize(24,24);
+        this.body.setOffset(-12,-12);
+        this.body.setCollideWorldBounds(true);
+        this.setDepth(2);
 
         this.#shipSprite = scene.add.sprite(0, 0, 'ship');
         this.#shipEngineSprite = scene.add.sprite(0, 0, 'ship_engine');
         this.#shipEngineThrustSprite = scene.add.sprite(0, 0, 'ship_engine_thruster');
         this.#shipEngineThrustSprite.play('ship_engine_thruster');
         this.add([this.#shipEngineThrustSprite, this.#shipEngineSprite, this.#shipSprite]);
+
+        this.#keyboardInputComponent = new KeyboardInputComponent(this.scene);
+        this.#horizontalMovementComponent = new HorizontalMovementComponent(this, this.#keyboardInputComponent, CONFIG.PLAYER_MOVEMENT_HORIZONTAL_VELOCITY);
+
+        this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
+        this.once(Phaser.GameObjects.Events.DESTROY, () => {
+            this.scene.events.off(Phaser.Scenes.Events.UPDATE, this.update, this);
+        }, this);
+    }
+
+    update(ts, dt) {
+        this.#keyboardInputComponent.update();
+        this.#horizontalMovementComponent.update();
     }
 }
