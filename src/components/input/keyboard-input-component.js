f86b0b835d09d6808e3aeaf61421529cd353660d
pedrogoliv <151693356+pedrogoliv@users.noreply.github.com>
2025-06-19 23:04:20 +0100

    mexer no A/D e disparar mouse1

--- src/components/input/keyboard-input-component.js
+++ src/components/input/keyboard-input-component.js
@@ -3,10 +3,16 @@ import { InputComponent } from './input-component.js';
 export class KeyboardInputComponent extends InputComponent {
   #cursorKeys;
   #inputLocked;
+  #aKey;
+  #dKey;
+  #pointer; // rato
 
   constructor(scene) {
     super();
     this.#cursorKeys = scene.input.keyboard.createCursorKeys();
+    this.#aKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.#dKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.#pointer = scene.input.activePointer;
     this.#inputLocked = false;
   }
 
@@ -20,10 +26,13 @@ export class KeyboardInputComponent extends InputComponent {
       return;
     }
 
+    // Movimento: setas OU A/D
     this._up = this.#cursorKeys.up.isDown;
     this._down = this.#cursorKeys.down.isDown;
-    this._left = this.#cursorKeys.left.isDown;
-    this._right = this.#cursorKeys.right.isDown;
-    this._shoot = this.#cursorKeys.space.isDown;
+    this._left = this.#cursorKeys.left.isDown || this.#aKey.isDown;
+    this._right = this.#cursorKeys.right.isDown || this.#dKey.isDown;
+
+    // Disparo: Espaço OU Clique esquerdo
+    this._shoot = this.#cursorKeys.space.isDown || this.#pointer.isDown;
   }
 }
